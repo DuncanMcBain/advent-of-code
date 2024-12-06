8ac54c90fdc56a1489c99cd3ccd4d3e9c4dd680d
Duncan McBain <git@mail.duncanmcba.in>
2024-12-06 18:15:56 +0100

    Day 3 complet

--- 2024/day3.d
+++ 2024/day3.d
@@ -1,4 +1,5 @@
 import std.algorithm;
+import std.algorithm.searching;
 import std.conv;
 import std.functional;
 import std.range;
@@ -18,6 +19,25 @@ int main() {
       accum += (to!int(first) * to!int(second));
     }
   }
+  writeln(accum);
+  accum = 0;
+  auto flip = (string a) => a == "do()" ? "don't()" : "do()";
+  auto needle = "don't()";
+  auto oneline = lines.join(" ");
+  do {
+    auto tup = findSplit(oneline, needle);
+    // found a match - empty if missing - finding "don't" means we are in "do"
+    if (tup[1] == "don't()" || (tup[2].empty && needle == "don't()")) {
+      auto matches = matchAll(tup[0], reg);
+      foreach(match; matches) {
+        auto first = match[1];
+        auto second = match[2];
+        accum += (to!int(first) * to!int(second));
+      }
+    }
+    oneline = tup[2];
+    needle = flip(needle);
+  } while (oneline.length);
   writeln(accum);
   return 0;
 }
